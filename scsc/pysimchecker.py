1ccbff9830c2f86822bb58338f663213f728fa21
Edson Eddy <crew0eddy@gmail.com>
2025-06-01 22:32:51 -0400

    Fix: Temporarily disable edit distance calculation in get_similarity_coefficient method

--- scsc/pysimchecker.py
+++ scsc/pysimchecker.py
@@ -96,7 +96,9 @@ class PySimChecker():
         return output
 
     def get_similarity_coefficient(self, proccesed_code1, proccesed_code2):
-        edit_distance = self.calculate_edit_distance(proccesed_code1, proccesed_code2)
+        # TODO: Fix the pysimchecker method
+        #edit_distance = self.calculate_edit_distance(proccesed_code1, proccesed_code2)
+        edit_distance = 0
         len_a = len(proccesed_code1)
         len_b = len(proccesed_code2)
         
