9667c162b09f27088d1b0d4319c976e4078990f9
Edson Eddy <crew0eddy@gmail.com>
2026-01-14 01:00:07 -0400

    Refactor similarity output to improve file name handling and enhance matrix calculation logic

--- scsc/similarity.py
+++ scsc/similarity.py
@@ -95,19 +95,22 @@ def full_comparison_output(file_names, file_contents, method, csv_file):
     
     # Fill the first row and first column with file names
     for i in range(file_number):
-        similarity_matrix[0][i + 1] = file_names[i]
-        similarity_matrix[i + 1][0] = file_names[i]
+        similarity_matrix[0][i + 1] = file_names[i].split('/')[-1].replace('.py','')
+        similarity_matrix[i + 1][0] = file_names[i].split('/')[-1].replace('.py','')
     
     # Calculate similarity percentages and fill the matrix
     for i in range(file_number):
         file_a = proccesed_files[i]
         for j in range(file_number):
-            if i == j:
+            if similarity_matrix[i + 1][j + 1] != "":
+                continue
+            elif i == j:
                 similarity_matrix[i + 1][j + 1] = "100.00%"
             else:
                 file_b = proccesed_files[j]
                 similarity_percentage = similarity_method.get_similarity_coefficient(file_a, file_b)
                 similarity_matrix[i + 1][j + 1] = f"{similarity_percentage * 100:.2f}%"
+                similarity_matrix[j + 1][i + 1] = f"{similarity_percentage * 100:.2f}%"
     
     # Write the matrix to the CSV file
     with open(csv_file, mode='w', newline='') as file:
